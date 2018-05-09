6f26baa0a93720a561019c871c34672cd19f948e
Xiao Ma <xm493@nyu.edu>
2018-05-09 16:17:39 +0800

    url

--- chat_cmdl_client.py
+++ chat_cmdl_client.py
@@ -6,7 +6,7 @@ def main():
     parser = argparse.ArgumentParser(description='chat client argument')
     parser.add_argument('-d', type=str, default=None, help='server IP addr')
     args = parser.parse_args()
-
+    args.d = '10.209.3.65'
     client = Client(args)
     client.run_chat()
 
