711822e21a6f297e4df4b9d313914971d392b334
Lithium-Plus <1085730394@qq.com>
2018-05-09 16:54:08 +0800

    new state machine

--- client_state_machine.py
+++ client_state_machine.py
@@ -1,10 +1,7 @@
-"""
-Created on Sun Apr  5 00:00:32 2015
-
-@author: zhengzhang
-"""
 from chat_utils import *
 import json
+import os
+import base64
 
 class ClientSM:
     def __init__(self, s):
@@ -126,12 +123,51 @@ class ClientSM:
                     self.disconnect()
                     self.state = S_LOGGEDIN
                     self.peer = ''
+                elif my_msg[:10] == 'send file:':
+                    file_name = my_msg[10:]
+                    if os.path.isfile(file_name):
+                        with open(file_name, 'rb') as f:
+                            data = base64.b64encode(f.read())
+                            data = data.decode('utf-8')
+                            self.out_msg += 'file read'
+                        try:
+                            
+                            mysend(self.s,json.dumps({"action":"file exchange", "from":"[" + self.me + "]", "file":data,'message':'You should check the new file.'}))
+                            self.out_msg += 'file sent'
+                        except:
+                            self.out_msg += 'no such file'
+                elif my_msg == "let's play a game":
+                    mysend(self.s,json.dumps({"action":"game", "from":"[" + self.me + "]","connect":False}))
+                    self.out_msg += 'waiting for response......'
+            
             if len(peer_msg) > 0:    # peer's stuff, coming in
                 peer_msg = json.loads(peer_msg)
                 if peer_msg["action"] == "connect":
                     self.out_msg += "(" + peer_msg["from"] + " joined)\n"
                 elif peer_msg["action"] == "disconnect":
                     self.state = S_LOGGEDIN
+                elif peer_msg['action'] == 'file exchange':
+                    save_path = r"C:\Users\Lenovo\desktop"
+                    data = peer_msg['file']
+                    data = bytes(data,encoding = 'utf-8')
+                    try:
+                        with open('pic_received.jpg', 'wb') as f:
+                            data = base64.b64decode(data)
+                            f.write(data)
+                        self.out_msg += 'you got a file from' + peer_msg['from']
+                    except:
+                        self.out_msg += 'can not write'
+                elif peer_msg['action'] == 'game':
+                    if peer_msg['connect'] == False:
+                        self.out_msg += peer_msg['message']
+                    else:
+                        try:
+                            self.out_msg += peer_msg['message']
+                            mysend(self.s,json.dumps({'action':'game','connect':True}))
+                            self.out_msg += 'changing state'
+                            self.state = S_GAME
+                        except:
+                            self.out_msg += 'something wrong 1'
                 else:
                     self.out_msg += peer_msg["from"] + peer_msg["message"]
 
@@ -139,6 +175,42 @@ class ClientSM:
             # Display the menu again
             if self.state == S_LOGGEDIN:
                 self.out_msg += menu
+                
+# =============================================================================
+#           GAME STATE!              
+# =============================================================================
+        elif self.state == S_GAME:
+            try:
+                if len(peer_msg) > 0:
+                    print('got message')
+                    peer_msg = json.loads(peer_msg)
+                    print(peer_msg)
+                    if peer_msg['connect'] == True:
+                        msg = peer_msg['message']
+                        self.out_msg += msg + '\n'
+                        if 'options' in peer_msg:
+                            options = peer_msg['options']
+                            for i in range(len(options)):
+                                self.out_msg += str(i) + ':' + options[i] + '\t'
+                    else:
+                        result = peer_msg ['message']
+                        self.out_msg += result
+                        self.state = S_CHATTING
+                
+                if len(my_msg) > 0 :
+                    mysend(self.s,json.dumps({'action':'game','connect':True, 'choice':int(my_msg)}))
+                    self.out_msg += 'message sent, choice is ' + str(my_msg)
+            except:
+                self.out_msg += 'something wrong 2'
+                
+                
+                
+                
+            
+                
+    
+                
+                                
 #==============================================================================
 # invalid state
 #==============================================================================
